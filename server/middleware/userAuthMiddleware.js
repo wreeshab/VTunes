c140376e4ccfe4d76087839ee0a1e2968a0c9f0f
wreeshab <evrishab@gmail.com>
2024-06-29 20:21:46 +0530

    create authmiddleware for user

---
+++ server/middleware/userAuthMiddleware.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+
+const userAuthMiddleware = (req, res, next) => {
+  const token = req.headers("Authorization").replace("Bearer ", "");
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Unauthorized User", success: false });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userID = decoded;
+    next();
+  } catch (err) {
+    console.log("not able to verify user ", err);
+    return res
+      .status(401)
+      .json({ message: "Unauthorized User/ Invalid Token", success: false });
+  }
+
+};
+
+export default userAuthMiddleware;
